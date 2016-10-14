821612abdf878aed982048e586c19d56163b5de1
Demis Bellot <demis.bellot@gmail.com>
2016-10-14 14:16:28 -0400

    Merge Program into Startup.cs

--- src/Imgur/Startup.cs
+++ src/Imgur/Startup.cs
@@ -13,9 +13,24 @@ using System.Drawing.Drawing2D;
 using ServiceStack.Text;
 using ServiceStack.VirtualPath;
 
-//Entire C# source code for ImageResizer backend - there is no other .cs :)
+//Entire C# source code for Imgur backend - there is no other .cs :)
 namespace Imgur
 {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
